a7148540defb57d3c044d5cd54160b21dc220bcb
Eslam-Ezz <Eslam_ezz93@outlook.com>
2025-01-23 10:54:03 +0200

    WDW

--- test_cases/test_login.py
+++ test_cases/test_login.py
@@ -6,35 +6,31 @@ import pytest
 from test_cases.config import driver
 
 
-
 class Test_login:
 
-        def test_login_valid(self,driver):
-                login_page = LoginPage(driver)
-                login_page.enter_username("eslam@outlook.com")
-                login_page.enter_password("12345678")
-                login_page.click_login()
-                time.sleep(2)
+    def test_login_valid(self, driver):
+        login_page = LoginPage(driver)
+        login_page.enter_username("eslam@outlook.com")
+        login_page.enter_password("12345678")
+        login_page.click_login()
+        time.sleep(2)NVG
 
+    def test_login_invalid_username(self, driver):
+        login_page = LoginPage(driver)
+        login_page.enter_username("eslam5875@outlook.com")
+        login_page.enter_password("12345678")
+        login_page.click_login()
+        time.sleep(2)
+        login_page.enter_username("eslam@outlook.com")
 
-        def test_login_invalid_username(self,driver):
-                login_page = LoginPage(driver)
-                login_page.enter_username("eslam5875@outlook.com")
-                login_page.enter_password("12345678")
-                login_page.click_login()
-                time.sleep(2)
-                login_page.enter_username("eslam@outlook.com")
+    def test_login_invalid_password(self, driver):
+        login_page = LoginPage(driver)
+        login_page.enter_username("eslam@outlook.com")
+        login_page.enter_password("123456723268")
+        login_page.click_login()
+        time.sleep(2)
 
-
-        def test_login_invalid_password(self,driver):
-                login_page = LoginPage(driver)
-                login_page.enter_username("eslam@outlook.com")
-                login_page.enter_password("123456723268")
-                login_page.click_login()
-                time.sleep(2)
-
-
-        def test_login_without_data(self,driver):
-                login_page = LoginPage(driver)
-                login_page.click_login()
-                time.sleep(2)
+    def test_login_without_data(self, driver):
+        login_page = LoginPage(driver)
+        login_page.click_login()
+        time.sleep(2)
